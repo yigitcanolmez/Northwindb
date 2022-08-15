8a2cfcbd5b0f895bc8db794d3424d3d3a27b74b8
Yiğitcan Ölmez <yigitcan.olmez@affinitybox.com>
2022-08-15 11:27:04 +0300

    Context

---
+++ DataAccess/Concrete/EntityFramework/EfProductDal.cs
@@ -0,0 +1,17 @@
+﻿using Core.DataAccess;
+using Core.DataAccess.EntityFramework;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework.Contexts;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfProductDal : EfEntityRepositoryBase<Product,NorthwindContext>,IProductDal
+    {
+        
+    }
+}
