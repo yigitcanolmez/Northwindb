34a5518b094819357a1424f9561ce10b6d04e81a
Yiğitcan Ölmez <yigitcan.olmez@affinitybox.com>
2022-08-17 10:59:02 +0300

    Business Layer katmanı oluşturuldu

---
+++ Business/Concrete/Contants/Messages.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete.Contants
+{
+    public static class Messages
+    {
+        public static string ProductAdded = "Ürün başarılı bir şekilde eklendi.";
+        public static string ProductDeleted = "Ürün başarılı bir şekilde silindi.";
+        public static string ProductUpdated = "Ürün başarılı bir şekilde güncellendi";
+
+    }
+}
