0f8cdb48a642251b5b46c8d9ebf3ebed3716622c
Yiğitcan Ölmez <yigitcan.olmez@affinitybox.com>
2022-08-19 09:37:37 +0300

    BGK-12

---
+++ Business/Concrete/UserManager.cs
@@ -0,0 +1,39 @@
+﻿using Business.Abstract;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Business.Concrete
+{
+    public class UserManager : IUserService
+    {
+        IUserDal _userDal;
+
+        public UserManager(IUserDal userDal)
+        {
+            _userDal = userDal;
+        }
+
+        public void Add(User user)
+        {
+            _userDal.Add(user);
+        }
+
+        public User GetByMail(string Mail)
+        {
+            return _userDal.Get(u=>u.Email == Mail);    
+        }
+
+        public List<OperationClaim> GetClaims(User user)
+        {
+            return new List<OperationClaim>((IEnumerable<OperationClaim>)_userDal.GetClaims(user));
+
+        }
+    }
+}
--- Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
+++ Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -15,8 +15,12 @@ namespace Business.DependencyResolvers.Autofac
         {
             builder.RegisterType<ProductManager>().As<IProductService>();
             builder.RegisterType<EfProductDal>().As <IProductDal>();
+
             builder.RegisterType<CategoryManager>().As<ICategoryService>();
             builder.RegisterType<EfCategoryDal>().As <ICategoryDal>();
+
+            builder.RegisterType<UserManager>().As<IUserService>();
+            builder.RegisterType<EfUserDal>().As <IUserDal>();
         }
     }
 }
